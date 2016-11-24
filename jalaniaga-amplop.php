55b3d9a38c6add5744035936dd596e0726df0ae5
Luthfi Emka <luthfi.emka@gmail.com>
2016-11-24 10:45:19 +0700

    add koneksi & data dr database

--- jalaniaga-amplop.php
+++ jalaniaga-amplop.php
@@ -1,4 +1,9 @@
 <?php
+include "ezsql.php";
+$thisdb         = new ezsql_mysqli('root','','jalaniaga','192.168.56.12');
+$param = $argv[1];
+$data = $thisdb->get_row("SELECT * FROM transaksi WHERE id = '$param' LIMIT 1");
+
 error_reporting(~E_WARNING & ~E_NOTICE);
 /* Call this file 'hello-world.php' */
 require __DIR__ . '/autoload.php';
@@ -11,10 +16,9 @@ $printer = new Printer($connector);
 $printer->setJustification(Printer::JUSTIFY_LEFT);
 $printer->text("Kepada Yth. \n");
 $printer->setTextSize(2,1);
-$printer->text("Ichsan Zuhasdi \n");
+$printer->text($data->nama_pembeli."\n");
 $printer->setTextSize(1,1);
-$printer->text("d.a Wisno Grahakom \n");
-$printer->text("Jl. Diponegoro 62\nJetis Kota Yogyakarta, 55000\nD.I. Yogyakarta\nTelp: 08122799372 \n");
+$printer->text($data->alamat." \n");
 $printer->text("\n\n");
 $printer->text("=========================== \n");
 $printer->text("Dari: \n");
--- jalaniaga.php
+++ jalaniaga.php
@@ -1,4 +1,9 @@
 <?php
+include "ezsql.php";
+$thisdb         = new ezsql_mysqli('root','','jalaniaga','192.168.56.12');
+$param = $argv[1];
+$data = $thisdb->get_row("SELECT * FROM transaksi WHERE id = '$param' LIMIT 1");
+$data_produk = $thisdb->get_results("SELECT * FROM produk WHERE id IN ($data->list_produk)");
 error_reporting(~E_WARNING & ~E_NOTICE);
 /* Call this file 'hello-world.php' */
 require __DIR__ . '/autoload.php';
@@ -21,32 +26,32 @@ $printer->setJustification(Printer::JUSTIFY_LEFT);
 $printer->text("ID. Transaksi:");
 $printer->text("\n");
 $printer->setPrintLeftMargin(128);
-$printer->text("INV/20161123/XVI/XI/58897707 [TOKOPEDIA]");
+$printer->text($data->id_transaksi_lain." [".strtoupper($data->vendor)."]");
 $printer->text("\n\n");
 $printer->setPrintLeftMargin(0);
-$arr_item = array(
-			'Orange Pi Zero H2'		            => 195000,
-			'Ongkir + Asuransi'			        => 10050
-			);
-			
-foreach(array_keys($arr_item) as $k)
+foreach($data_produk as $k)
 {
 	$printer->setPrintLeftMargin(0);
-	$printer->text($k);
+	$printer->text($k->nama_barang);
 	$printer->text("\n");
 	$printer->setPrintLeftMargin(256);
-	$printer->text($arr_item[$k]);
+	$printer->text($k->rp_harga);
 	$printer->text("\n");
-	
-	$tot += $arr_item[$k];
 }
+$printer->setPrintLeftMargin(0);
+$printer->text('Ongkir, dll');
+$printer->text("\n");
+$printer->setPrintLeftMargin(256);
+$printer->text($data->rp_ongkir);
+$printer->text("\n");
+
 $printer->setPrintLeftMargin(0);
 $printer->text("------------------------------ +");
 $printer->text("\n");
 $printer->text("Total");
 $printer->text("\n");
 $printer->setPrintLeftMargin(256);
-$printer->text($tot.",-");
+$printer->text($data->rp_total.",-");
 $printer->text("\n");
 $printer->text("\n");
 $printer->setJustification(Printer::JUSTIFY_CENTER);
@@ -57,9 +62,8 @@ $printer->text( strftime('%c') );
 $printer->text("\n\n");
 $printer->text("===============================\n\n");
 $printer->setJustification(Printer::JUSTIFY_LEFT);
-$printer->text("Ichsan Zuhasdi \n");
-$printer->text("d.a Wisno Grahakom \n");
-$printer->text("Jl. Diponegoro 62\nJetis Kota Yogyakarta, 55000\nD.I. Yogyakarta\nTelp: 08122799372 \n");
+$printer->text($data->nama_pembeli." \n");
+$printer->text($data->alamat." \n");
 $printer->text("\n\n");
 $printer->text("OS Orange Pi Zero dapat didownload di: http://www.orangepi.org/downloadresources/.\n\n");
 $printer->setJustification(Printer::JUSTIFY_CENTER);
